38bb2aba7c43fdae3caa85bf6e04f524961c41c5
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2024-12-29 15:03:02 +0100

    Fiddle with codecov

--- src/backend/tests.sh
+++ src/backend/tests.sh
@@ -5,7 +5,9 @@ dotnet test --collect="XPlat Code Coverage" \
     --settings coverage.runsettings \
     --results-directory /results/coverage
 
+# TODO: Test if codecov prefers lcov or Cobertura.
 ./reportgenerator/reportgenerator \
     "-reports:/results/coverage/*/*.xml" \
      "-reporttypes:lcov" \
      "-targetdir:/results/report"
+    #  "-reporttypes:lcov;Cobertura" \
