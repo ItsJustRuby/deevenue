f2dcb704c38a2a430805503f62590f0579af356e
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2024-12-31 06:19:12 +0100

    Remove TODO.

--- src/backend/Deevenue.Api/EntityKind.cs
+++ src/backend/Deevenue.Api/EntityKind.cs
@@ -6,6 +6,5 @@ namespace Deevenue.Api;
 [JsonConverter(typeof(LowercaseJsonStringEnumConverter))]
 public enum EntityKind
 {
-    // TODO: OK this enum still only has one value. Either use it way more or way less.
     Medium
 }
