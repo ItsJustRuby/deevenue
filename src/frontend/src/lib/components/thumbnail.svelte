7796f5db116d0a4bed10fcb85fb62744453d4005
ItsJustRuby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2025-01-04 06:04:11 +0100

    fix(ui): Use different preloading strategy and doc it. (#24)

--- src/frontend/src/lib/components/thumbnail.svelte
+++ src/frontend/src/lib/components/thumbnail.svelte
@@ -11,7 +11,6 @@
   let pictureRef = $state<HTMLDivElement | undefined>(undefined);
 
   const startVideo = async () => {
-    pictureRef!.classList.add("opacity-0");
     missingThumbnailRef?.classList.remove("opacity-0");
     try {
       await videoRef!.play();
@@ -19,6 +18,10 @@
     } catch (e: unknown) {
       // Explicitly fine if the video src is 404.
     }
+    // Only do this after awaiting the play() - that initially loads the
+    // video's src. If you make the pictureRef invisible before then,
+    // you get a (white) flash of the background shining through.
+    pictureRef!.classList.add("opacity-0");
   };
   const rewindVideo = async () => {
     pictureRef!.classList.remove("opacity-0");
@@ -36,12 +39,15 @@
   let videoSrc = $derived(
     backendUrl(`/thumbnail/${id}_${mediaQuery.current ? "l" : "s"}.webm`),
   );
+  let imgSrc = $derived(
+    backendUrl(`/thumbnail/${id}_${mediaQuery.current ? "l" : "s"}.jpg`),
+  );
 
   let missingThumbnailRef = $state<HTMLDivElement | null>(null);
 
   const on404 = () => {
-    videoRef?.classList.add("hidden");
     missingThumbnailRef?.classList.remove("hidden");
+    videoRef?.classList.add("hidden");
   };
 </script>
 
@@ -54,6 +60,17 @@
     {:else if mediaKind === "video"}
       <div class="grid grid-cols-1 grid-rows-1">
         <div class="col-start-1 row-start-1 h-full w-full">
+          <!--
+          preload="none" is strongly encouraged:
+          * preload="auto" would preload all video thumbnails on screen, regardless
+            whether the user can even see them (e.g. if they are on mobile).
+          * not specifying preload at all makes the browser decide what it wants,
+            and it is often an idiot about prioritizing media requests. 
+          
+          However, by not preloading the video src, we *do* need to preload the `poster`:
+          Without that, the browser does not know what height this div should have
+          (width is determined by the containing column).
+          -->
           <video
             class="h-full w-full"
             src={videoSrc}
@@ -62,7 +79,8 @@
             loop={true}
             muted={true}
             playsInline={true}
-            preload="auto"
+            preload="none"
+            poster={imgSrc}
             disablepictureinpicture
           >
           </video>
